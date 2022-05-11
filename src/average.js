5ebfecc101bdf4e76953aa8cb784e60d50d4fbd1
Sally Diament <sally.barzilai@gmail.com>
2022-05-11 15:09:44 -0300

    cumprindo o requisito 1

--- src/average.js
+++ src/average.js
@@ -11,8 +11,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  return 1 + 1;
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let summation = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    } 
+    summation += array[i]; 
+  }
+  const media = summation / array.length; 
+  return Math.round(media);
 };
+
+console.log(average([2, 4]));
 
 module.exports = average;
